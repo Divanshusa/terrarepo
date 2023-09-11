9396f24e1e5d9dce6a02455542b4f3568d16a4ec
dishu <dishu@gmail.com>
2023-09-11 18:07:01 +0530

    my first code

---
+++ new.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+ profile = "default"
+ region = "ap-south-1"
+}
+
+resource "aws_vpc" "terravpc" {
+ cidr_block = "10.0.0.0/16"
+ tags = {
+  Name = "terravpc"
+ }
+}
+
+resource "aws_subnet" "terrasub" {
+ vpc_id = aws_vpc.terravpc.id
+ availability_zone = "ap-south-1a" 
+ cidr_block = "10.0.1.0/24"
+ tags = {
+  Name = "terrasub1"
+ }
+}
